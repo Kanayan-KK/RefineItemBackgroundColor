44494b0889d63839a2c530e885775ed3023607fa
Kanayan <glgln6@gmail.com>
2025-12-14 19:44:44 +0900

    v1.0.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# RefineItemBackgroundColor
+Elin Mod
+
+[SteamWorkShop](https://steamcommunity.com/sharedfiles/filedetails/?id=3623674433)
+
+## License
+
+MIT
---
+++ src/Patch.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RefineItemBackgroundColor
+{
+    [HarmonyPatch]
+    public static class Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Trait), nameof(Trait.OnSetCardGrid))]
+        public static void OnSetCardGrid_Postfix(Trait __instance, ButtonGrid __0)
+        {
+            var card = __0.Card;
+
+            // 食べ物アイテムを除外
+            if (card.IsFood)
+                return;
+
+            // エンチャントが無いアイテムを除外
+            if (card.elements == null)
+                return;
+
+            var images = __0.GetComponentsInChildren<Image>(true);
+
+            switch (images.Length)
+            {
+                case 0:
+                    return;
+                case > 0:
+                {
+                    var targetImage = images.FirstOrDefault(image =>
+                        image.canvasRenderer.gameObject.name.Contains("ButtonGrid"));
+                    if (targetImage != null)
+                    {
+                        var outline = targetImage.GetComponent<Outline>();
+
+                        if (outline == null)
+                            outline = targetImage.gameObject.AddComponent<Outline>();
+
+                        outline.effectColor = Color.white;
+                        outline.effectDistance = new Vector2(2, -2);
+                        outline.enabled = true;
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+};
